00cf11288bad42713b53f938494586147c64f1ef
YashDhanraj <yashdhanraj9140@gmail.com>
2025-09-26 11:43:40 +0530

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',              //  important for static hosting
   plugins: [react()],
   server: { port: 5173 }
 })
-
-
